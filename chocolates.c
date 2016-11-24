82c1df6ec5beeeff93b5f669487fd82b55218613
Gio <giodaniel04@hotmail.com>
2016-11-23 23:52:33 -0600

    chocolates

---
+++ chocolates.c
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+main ()
+{
+
+         int chocolates [10];
+         int i;
+         int suma=0;
+         for(i=0;i<10;i++)
+         cout<<"\n ingrese un valor para el arreglo";
+         cin>>chocolates[i];
+         suma=suma+chocolates[i];
+         }
+
+         cout<<"\n\n La suma de los chocolates es"<<suma;
+         cout<<"\n\n\n";
+
+system ("PAUSE");
+return 0;
+}
